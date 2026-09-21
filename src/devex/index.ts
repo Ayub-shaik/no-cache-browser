c9c6b77c195a4d78cf74e5351898a7b25bd6c554
SHAIK AYUB MOHIDDIN <56859951+Ayub-shaik@users.noreply.github.com>
2026-09-21 21:10:36 +0530

    feat(devex): add SessionBuffer/panel exports

---
+++ src/devex/index.ts
@@ -0,0 +1,9 @@
+export {
+  DEFAULT_BODY_CAP,
+  DEFAULT_CONSOLE_CAP,
+  SessionBuffer,
+  type ConsoleEntry,
+  type SessionBufferMeta,
+  type SessionTabMeta,
+} from "./session-buffer.js";
+export { attachDevExPanel, type DevExPanelOptions } from "./panel.js";
