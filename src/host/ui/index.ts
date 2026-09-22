89917fcd83e06a0b63d61defa523d7f66c1c0140
SHAIK AYUB MOHIDDIN <56859951+Ayub-shaik@users.noreply.github.com>
2026-09-22 14:36:41 +0530

    host: add NCB product window UI module

---
+++ src/host/ui/index.ts
@@ -0,0 +1,12 @@
+export {
+  startHostUiServer,
+  normalizeUrl,
+  type HostUiServer,
+  type HostUiState,
+  type HostUiServerHandlers,
+} from "./server.js";
+export {
+  ContentController,
+  type ContentControllerOptions,
+  type TabSummary,
+} from "./controller.js";
